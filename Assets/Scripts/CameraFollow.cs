8cb8a367b692f593df10029ccd94ca2725ab3848
Nguyễn Thành Danh <17520323@ms.uit.edu.vn>
2020-10-27 09:05:17 +0700

    first commit

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    // Use this for initialization
+    public Transform target;
+    public float smoothing;
+    Vector3 offset;
+	void Start () {
+        offset = transform.position - target.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void FixedUpdate()
+    {
+        Vector3 targetCampos = target.position + offset;
+        transform.position = Vector3.Lerp(transform.position, targetCampos, smoothing * Time.deltaTime);
+    }
+     
+}
---
+++ Assets/Scripts/Managers/GameOverManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class GameOverManager : MonoBehaviour
+{
+    public PlayerHealth playerHealth;
+    public float restartDelay = 5f;
+    public Text scoreText;
+
+
+    Animator anim;
+    float restartTimer;
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+        
+    }
+
+
+    void Update()
+    {
+        if (playerHealth.currentHealth <= 0)
+        {
+            restartTimer += Time.deltaTime;
+            anim.SetTrigger("GameOver");
+            if (restartTimer >= restartDelay)
+            {
+                SceneManager.LoadScene("Level01");
+            }
+        }
+    }
+}
